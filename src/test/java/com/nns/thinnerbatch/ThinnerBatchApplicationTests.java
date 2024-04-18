4145e6b8f7b7c4ab24c5cabb47bb866c340f1ca6
yujin.lee <dbwksl128@gmail.com>
2024-04-18 16:23:29 +0900

    init

---
+++ src/test/java/com/nns/thinnerbatch/ThinnerBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nns.thinnerbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinnerBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
